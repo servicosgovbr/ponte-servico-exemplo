584f32e751c9e4acc31b32216ab09cfde9372143
Carlos Villela <cvillela@thoughtworks.com>
2015-08-04 23:39:45 -0300

    Commit inicial

---
+++ src/main/java/br/gov/servicos/ponte/Main.java
@@ -0,0 +1,13 @@
+package br.gov.servicos.ponte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
+
